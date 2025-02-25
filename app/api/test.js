a37643c47fe06b97c7d70d35f5e0e6aa434e30dd
Galib24 <abuyeahia24@gmail.com>
2025-02-25 19:07:28 +0600

    test

---
+++ app/api/test.js
@@ -0,0 +1,10 @@
+import pool from '@/libs/db';
+
+export default async function handler(req, res) {
+  try {
+    const [rows] = await pool.query("SELECT NOW() AS currentTime");
+    res.status(200).json({ message: "Connected to MySQL!", time: rows[0].currentTime });
+  } catch (error) {
+    res.status(500).json({ error: "Database connection failed", details: error.message });
+  }
+}
