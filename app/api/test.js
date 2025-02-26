38aaa1ec0d19b4d20996b2d48785a51448d61271
Galib24 <abuyeahia24@gmail.com>
2025-02-26 12:06:16 +0600

    added

--- app/api/test.js
+++ app/api/test.js
@@ -7,4 +7,6 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ error: "Database connection failed", details: error.message });
   }
-}
+}  
+
+// juwbghruin
