f438493627c4dbfa4465bfaf31ff7bcb268347a8
Galib24 <abuyeahia24@gmail.com>
2025-03-02 15:18:43 +0600

    lmn

--- app/api/Dashboard_User/route.js
+++ app/api/Dashboard_User/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 // import db from "../../../libs/db";
-import db from "@/libs/db";
+import {db} from "../../../libs/db";
 
 export async function GET() {
   try {
