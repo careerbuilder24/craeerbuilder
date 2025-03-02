168c58e558d518a2fbcd199301d076385491974b
Galib24 <abuyeahia24@gmail.com>
2025-03-02 16:24:53 +0600

    fixxxx

--- app/api/Dashboard_User/route.js
+++ app/api/Dashboard_User/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
-// import db from "../../../libs/db";
-import {db} from "../../../libs/db";
+import db from "../../../libs/db";
 
 export async function GET() {
   try {
--- app/api/Login_users/route.js
+++ app/api/Login_users/route.js
@@ -1,4 +1,4 @@
-import { db } from "../../../libs/db"; // Import database connection
+import  db  from "../../../libs/db"; // Import database connection
 
 export default async function handler(req, res) {
     if (req.method === "GET") {
