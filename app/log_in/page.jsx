b442328d0d7ffdeb7d47401f6e1e216d9cc75311
Galib24 <abuyeahia24@gmail.com>
2025-03-03 15:35:16 +0600

    bm

--- app/log_in/page.jsx
+++ app/log_in/page.jsx
@@ -54,9 +54,15 @@ export default function Login() {
         const result = await response.json();
         console.log("Backend Response:", result);  // Log backend response
 
+        // if (!response.ok) {
+        //     throw new Error(result.message || "Login failed. Please check your credentials.");
+        // }
         if (!response.ok) {
-            throw new Error(result.message || "Login failed. Please check your credentials.");
-        }
+          console.error("Login error:", result);
+          toast.error(result?.message || "Login failed. Try again.");
+          return;
+      }
+      
 
         toast.success(result.message);
         loginUserManual(result.user);
