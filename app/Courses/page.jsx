f5071e1670f31d792f53132ca1e56ed0b88eae25
Galib24 <abuyeahia24@gmail.com>
2025-03-08 15:50:16 +0600

    search button fix

--- app/Courses/page.jsx
+++ app/Courses/page.jsx
@@ -100,7 +100,7 @@ export default function Courses() {
                 type="submit"
                 className="absolute inset-y-0 right-11 flex items-center px-4 text-white bg-blue-500 rounded hover:bg-blue-600"
               >
-                Search
+                Search...
               </button>
             </div>
 
@@ -156,7 +156,7 @@ export default function Courses() {
               type="submit"
               className="ml-2 px-6 py-2 text-white bg-[#17549A] rounded-md hover:bg-blue-600 transition duration-300 cursor-pointer"
             >
-              Search...
+              Search
             </button>
           </div>
 
