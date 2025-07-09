a4a953ece4f39d255edbc7e6d47dac1e2af53754
careerbuilder24 <careerbuilder.com.bd@gmail.com>
2025-07-09 18:44:31 +0600

    added part in study abroad

--- app/(with-navbar)/DetailsAbroad/[id]/page.jsx
+++ app/(with-navbar)/DetailsAbroad/[id]/page.jsx
@@ -1,6 +1,5 @@
-'use client'
+'use client';
 import Image from 'next/image';
-import Link from 'next/link';
 import Navbar from '../../componenets/Navbar/Navbar';
 import { useParams } from 'next/navigation';
 import useAbroadStudy from '@/hooks/useAbroadStudy';
@@ -11,99 +10,78 @@ export default function UniversityDetailPage() {
     const { id } = params;
     const { data } = useAbroadStudy();
 
-    // Flatten all universities into a single array
     const allUniversities = Object.values(data || {}).flat();
-
-    // Find the university by ID
     const university = allUniversities.find((u) => u.id === parseInt(id));
 
     if (!university) {
         return <div className='text-center mt-40 text-xl text-red-500'>University not found!</div>;
     }
 
-
     return (
         <>
             <Navbar />
-            <div className=' mx-auto lg:mt-36 container flex justify-center'>
 
+            {/* Main image */}
+            <div className='container mx-auto mt-28 lg:mt-36 flex justify-center px-4'>
                 <Image
                     src={university.imageOne}
                     width={900}
                     height={900}
-                    className='w-9/12 h-auto'
+                    className='w-full sm:w-10/12 lg:w-9/12 h-auto rounded-lg shadow-md'
                     alt='university main image'
-
                 />
-
-
             </div>
 
-            <div className='flex justify-center mt-10'>
-                <p className='w-6/12  text-justify font-semibold text-xl'>
+            {/* Description One */}
+            <div className='flex justify-center mt-10 px-4'>
+                <p className='w-full sm:w-10/12 md:w-8/12 lg:w-6/12 text-justify font-semibold text-lg sm:text-xl'>
                     {university.descriptionOne}
                 </p>
             </div>
 
-            {/* second part */}
-            <div className='flex justify-center   mt-20 '>
-
-
-                <p className='w-4/12  text-justify  font-semibold textarea-lg'>
+            {/* Description Two */}
+            <div className='flex flex-col-reverse lg:flex-row justify-center items-center mt-16 gap-8 px-4'>
+                <p className='w-full lg:w-5/12 text-justify font-semibold text-lg'>
                     {university.descriptionTwo}
                 </p>
-
                 <Image
                     src={university.imageTwo}
                     width={900}
                     height={900}
-                    className='w-3/12 h-auto'
+                    className='w-full sm:w-8/12 md:w-6/12 lg:w-3/12 h-auto rounded-md shadow'
                     alt='university second image'
-
                 />
             </div>
-            {/* third part */}
-            <div className='flex justify-center   mt-20 '>
 
-
+            {/* Description Three */}
+            <div className='flex flex-col lg:flex-row justify-center items-center mt-16 gap-8 px-4'>
                 <Image
                     src={university.imageThree}
                     width={900}
                     height={900}
-                    className='w-3/12 h-auto'
-                    alt='university second image'
-
+                    className='w-full sm:w-8/12 md:w-6/12 lg:w-3/12 h-auto rounded-md shadow'
+                    alt='university third image'
                 />
-
-                <p className='w-4/12  text-justify  font-semibold textarea-lg'>
+                <p className='w-full lg:w-5/12 text-justify font-semibold text-lg'>
                     {university.descriptionThree}
                 </p>
-
             </div>
-            {/* Fourth part */}
-            <div className='flex justify-center   mt-20  mb-10'>
-                <p className='w-4/12  text-justify  font-semibold textarea-lg'>
+
+            {/* Description Four */}
+            <div className='flex flex-col-reverse lg:flex-row justify-center items-center mt-16 mb-10 gap-8 px-4'>
+                <p className='w-full lg:w-5/12 text-justify font-semibold text-lg'>
                     {university.descriptionFour}
                 </p>
-
                 <Image
                     src={university.imageFour}
                     width={900}
                     height={900}
-                    className='w-3/12 h-auto'
-                    alt='university second image'
-
+                    className='w-full sm:w-8/12 md:w-6/12 lg:w-3/12 h-auto rounded-md shadow'
+                    alt='university fourth image'
                 />
-
-
-
             </div>
+
             <Footer />
-
-
-
         </>
-
     );
 }
-
