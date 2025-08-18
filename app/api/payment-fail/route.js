2ab1fcdb4de5f09d9c9fb97a54a48559cea8f451
careerbuilder24 <careerbuilder.com.bd@gmail.com>
2025-08-18 20:00:26 +0600

    fix fail payment

--- app/api/payment-fail/route.js
+++ app/api/payment-fail/route.js
@@ -1,10 +1,14 @@
-// // app/api/payment-fail/route.js
-// import { NextResponse } from "next/server";
+// //// app/api/payment-cancel/route.js
+import { NextResponse } from "next/server";
 
-// export async function POST() {
+export async function POST() {
+  // Force GET redirect instead of re-POST
   return NextResponse.redirect(
     `${process.env.NEXT_PUBLIC_APP_URL}/payment-fail`,
-    302  //  important: makes browser follow with GET
+    302  // 👈 important: makes browser follow with GET
   );
+}
+
+
 //   // Redirect to fail page after payment
 
